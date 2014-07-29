a003052e4db8a47c1c2b3b70e05d9c5daf3d222e
polohuang <shokai036@gmail.com>
2014-07-29 15:45:00 +0800

    20140729 1544

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => '140.117.71.65',
+			'database'  => 'laravel',
+			'username'  => 'limesurvey',
+			'password'  => '1029',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
